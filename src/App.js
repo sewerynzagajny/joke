0758de4747c76d5d6026ff88531ece54fe136dce
sewerynzagajny <seweryn.zagajny@gmail.com>
2024-10-18 00:39:56 +0200

    18_10_2024

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <JokingText />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return (
+    <div className="logo">
+      <img className="logo__img" src="/logo.png" alt="logo" />
+      <h1 className="logo__heading-primary"> Ale jaja!!</h1>
+    </div>
+  );
+}
+
+function JokingText() {
+  return (
+    <div className="joking-text">
+      <p className="setup">Why do cows wear bells?</p>
+      <p className="delivery">Because their horns don't work!</p>
+      <p className="joke">
+        I have a fish that can breakdance! Only for 20 seconds though, and only
+        once.
+      </p>
+    </div>
+  );
+}
