a046a554118fa3bdec4a389b5500e4b2d567f664
sewerynzagajny <seweryn.zagajny@gmail.com>
2024-10-19 00:46:40 +0200

    19_10_2024

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { useState } from "react";
+
+const language = [
+  { id: 0, value: "pl", descriptionPl: "Polski", descriptionEN: "Polish" },
+  { id: 1, value: "en", descriptionPl: "Angielski", descriptionEN: "English" },
+];
+
+function Button({ onClick, children }) {
+  return (
+    <button className="btn" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
 export default function App() {
   return (
     <div className="app">
       <Logo />
       <JokingText />
+      <Cta />
+      <Footer />
     </div>
   );
 }
@@ -26,5 +43,40 @@ function JokingText() {
         once.
       </p>
     </div>
+  );
+}
+
+function Cta() {
+  const [select, setStelect] = useState("pl");
+  return (
+    <div className="cta">
+      <Button>{select === "pl" ? "Żart" : "Joke"}</Button>
+      <select
+        className="cta__select"
+        value={select}
+        onChange={(e) => setStelect(e.target.value)}
+      >
+        <option value="" disabled selected>
+          {select === "pl" ? "Wybierz język" : "Select language"}
+        </option>
+        {language.map((el) => (
+          <option key={el.id} value={el.value}>
+            {select === "pl" ? el.descriptionPl : el.descriptionEN}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p className="copyright">
+        Copyright &copy; <span>{new Date().getFullYear()}</span> by Seweryn
+        Zagajny. <br />
+        All rights reserved.
+      </p>
+    </footer>
   );
 }
