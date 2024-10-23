6a8eebb57c6090506894043a0e9a5567e05d4d3f
sewerynzagajny <seweryn.zagajny@gmail.com>
2024-10-23 15:52:25 +0200

    ver4

--- netlify/functions/translate.js
+++ netlify/functions/translate.js
@@ -1,6 +1,6 @@
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
 
-exports.handler = async (event) => {
+export async function handler(event) {
   const { text } = JSON.parse(event.body);
 
   const apiURL = "https://api-free.deepl.com/v2/translate";
@@ -38,4 +38,4 @@ exports.handler = async (event) => {
       body: JSON.stringify({ error: error.message }),
     };
   }
-};
+}
