5fea3cc9479acc0a1b420ed8c588b56a5017dabc
sewerynzagajny <seweryn.zagajny@gmail.com>
2024-10-23 17:52:10 +0200

    ver07

--- netlify/functions/translate.js
+++ netlify/functions/translate.js
@@ -1,52 +1,10 @@
-// export async function handler(event) {
-//   const { text } = JSON.parse(event.body);
-
-//   const apiURL = "https://api-free.deepl.com/v2/translate";
-//   const apiKey = process.env.DEEPL_API_KEY;
-
-//   const fetch = (await import("node-fetch")).default;
-
-//   const payload = {
-//     text: text,
-//     source_lang: "EN",
-//     target_lang: "PL",
-//   };
-
-//   try {
-//     const response = await fetch(apiURL, {
-//       method: "POST",
-//       headers: {
-//         Authorization: `DeepL-Auth-Key ${apiKey}`,
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify(payload),
-//     });
-
-//     const data = await response.json();
-
-//     if (!response.ok) {
-//       throw new Error(`Błąd: ${response.status}`);
-//     }
-
-//     return {
-//       statusCode: 200,
-//       body: JSON.stringify(data),
-//     };
-//   } catch (error) {
-//     return {
-//       statusCode: 500,
-//       body: JSON.stringify({ error: error.message }),
-//     };
-//   }
-// }
-
 export async function handler(event) {
   console.log("Przybyłe ciało żądania:", event.body);
 
   if (!event.body) {
     return {
       statusCode: 400,
-      body: JSON.stringify({ error: "Brak ciała w żądaniu" }),
+      body: JSON.stringify({ error: "No body in task" }),
     };
   }
 
@@ -55,10 +13,10 @@ export async function handler(event) {
     const { text: bodyText } = JSON.parse(event.body);
     text = bodyText;
   } catch (error) {
-    console.error("Błąd parsowania JSON:", error);
+    console.error("Error parse JSON:", error);
     return {
       statusCode: 400,
-      body: JSON.stringify({ error: "Niepoprawny JSON" }),
+      body: JSON.stringify({ error: "Incorrect JSON" }),
     };
   }
 
@@ -85,7 +43,7 @@ export async function handler(event) {
 
     if (!response.ok) {
       throw new Error(
-        `Błąd: ${response.status} - ${data.message || "Nieznany błąd"}`
+        `Błąd: ${response.status} - ${data.message || "Unknown error"}`
       );
     }
 
@@ -94,7 +52,7 @@ export async function handler(event) {
       body: JSON.stringify(data),
     };
   } catch (error) {
-    console.error("Błąd w funkcji:", error);
+    console.error("Error in function:", error);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: error.message }),
